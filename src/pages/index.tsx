f3d00f777b3e1b9663d35aa069c1be4da97f0f11
João Maia <15joaomaia@gmail.com>
2022-07-20 12:04:54 -0300

    criando as paginas

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,24 @@
 import { NextPage } from 'next'
+import { useRouter } from 'next/router'
 
 const Home: NextPage = () => {
-  return <>Hello world</>
+  const router = useRouter()
+
+  const handlerLogin = () => {
+    router.push('/login')
+  }
+
+  const handlerRegister = () => {
+    router.push('/login')
+  }
+
+  return (
+    <>
+      <h1>Olá Mundo</h1>
+      <button onClick={handlerLogin}>Login</button>
+      <button onClick={handlerRegister}>Register</button>
+    </>
+  )
 }
 
 export default Home
